9158984379996f7af9a39cf522bd097c3e7fd184
EmekaOrji <emekapraiseo@gmail.com>
2022-06-20 14:59:25 -0700

    fix: task 08

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,5 +13,5 @@ int main(void)
 		 2,
 		 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
-	return (0);
+	return (1);
 }
