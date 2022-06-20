66a894b01f3c7febc1ddbe91d5cdf202f155ddf9
EmekaOrji <emekapraiseo@gmail.com>
2022-06-20 14:46:29 -0700

    chore: task 08

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", "string format", 30);
+	return (0);
+}
