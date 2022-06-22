71abfe54e526b26c0e62ffaec0fb683fda1a5d68
EmekaOrji <emekapraiseo@gmail.com>
2022-06-21 22:37:38 -0700

    chore: Task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers in base 10
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	for (int num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
