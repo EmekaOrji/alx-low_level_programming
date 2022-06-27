f05fb469e76aa3f1347be82b06ad6eada6c4626e
EmekaOrji <emekapraiseo@gmail.com>
2022-06-27 15:29:09 -0700

    Task 03

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for letter casing
+ *
+ * Return: 1 if lowercase and 0 if uppercase
+ */
+int _islower(int c)
+{
+	int letter;
+
+	if (letter >= 'a' && letter <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
