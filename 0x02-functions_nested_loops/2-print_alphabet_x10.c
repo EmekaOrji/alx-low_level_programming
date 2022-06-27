83b412a94cc9fb5a1687d3b0065659385cfb7de4
EmekaOrji <emekapraiseo@gmail.com>
2022-06-27 15:18:33 -0700

    Task 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * print_alphabet_x10 - entry point
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int letter;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
