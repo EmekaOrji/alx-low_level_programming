6c429872eecd1cf5d56731d2c4d0883c03039f08
EmekaOrji <emekapraiseo@gmail.com>
2022-06-28 04:06:15 -0700

    Followed the usual rules

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,23 +5,20 @@
  * @number: any integer input
  * @lastDigit: the last digit of the input integer
  * @absoluteNumber: the absolute value of the input integer
- * Return: the last digit of the integer input
+ * Return: the absolute value of the last digit of the integer input
  */
-int _abs(int);
-
 int print_last_digit(int number)
 {
 	int lastDigit;
 	int absoluteNumber;
 
 	lastDigit = (number % 10);
-	absoluteNumber = _abs(lastDigit);
+	
+	if (lastDigit < 0)
+		absoluteNumber = lastDigit * -1;
+	else
+		absoluteNumber = lastDigit;
 
 	_putchar(absoluteNumber);
 	return (absoluteNumber);
-}
-
-int _abs(int num)
-{
-	return (num * ((num > 0) - (num < 0)));
 }
