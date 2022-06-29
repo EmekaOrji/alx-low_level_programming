d361a37223856bfcfc2551d5d64a7f54eb985f65
Emeka Orji <emekapraiseo@gmail.com>
2022-06-29 21:21:00 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,45 +5,8 @@
  * @n: number of timetable to be printed
  * Return: void
  */
-void print_non_zeros(int n, int column, int row, int digit)
-{
-	for (row = 0; row <= n; row++)
-		{
-			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-			for (column = 1; column <= n; column++)
-			{
-				digit = (column * row);
-				if (digit <= 99)
-				{
-					if ((digit / 10) > 0)
-					{
-						_putchar(' ');
-						_putchar((digit / 10) + '0');
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar((digit % 10) + '0');
-				}
-				else
-				{
-					_putchar(((digit / 10) /10) + '0');
-					_putchar(((digit / 10) % 10) + '0');
-					_putchar((digit % 10) + '0');
-				}
-				if (column < n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			_putchar('\n');
-		}
-}
+void print_non_zeros(int, int, int, int);
+
 void print_times_table(int n)
 {
 	int column, row, digit;
@@ -59,5 +22,45 @@ void print_times_table(int n)
 		{
 			print_non_zeros(n, column, row, digit);
 		}
+	}
+}
+
+void print_non_zeros(int n, int column, int row, int digit)
+{
+	for (row = 0; row <= n; row++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (column = 1; column <= n; column++)
+		{
+			digit = (column * row);
+			if (digit <= 99)
+			{
+				if ((digit / 10) > 0)
+				{
+					_putchar(' ');
+					_putchar((digit / 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((digit % 10) + '0');
+			}
+			else
+			{
+				_putchar(((digit / 10) /10) + '0');
+				_putchar(((digit / 10) % 10) + '0');
+				_putchar((digit % 10) + '0');
+			}
+			if (column < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
 }
