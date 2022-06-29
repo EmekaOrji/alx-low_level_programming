cb7fde52df33d32ac2d166c847f96b43ec98df14
Emeka Orji <emekapraiseo@gmail.com>
2022-06-29 22:21:23 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,22 +5,22 @@
  *
  * Return: Always 0 (success)
  */
-int main()
+int main(void)
 {
- 	unsigned long n, i, j, k;
-  
+	unsigned long n, i, j, k;
+	
 	i = 0;
 	j = 1;
-  	for (n = 1; n <= 50; n++)
-  	{
-    	k = i + j;
-    	i = j;
-    	j = k;
-    	printf("%lu", k);
-    	if (n < 50)
-      		printf(", ");
-  	}
-  	printf("\n");
-  
-  	return (0);
+	for (n = 1; n <= 50; n++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+		if (n < 50)
+			printf(", ");
+	}
+	printf("\n");
+	
+	return (0);
 }
