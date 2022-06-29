b380ddc3137791582caf2dbde4ef32329252024d
EmekaOrji <emekapraiseo@gmail.com>
2022-06-29 06:17:38 -0700

    fix: still pretty betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,7 @@
 
 /**
  * add - adds two integers
- * num1: inputed integer 1
- * num2: inputed integer 2
+ * @num1, @num2: inputed integers to be added
  * Return: the result of the addition
  */
 int add(int num1, int num2)
