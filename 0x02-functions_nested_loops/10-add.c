9e7876c24f3159ad18f0fa459fc48d1dcd09d6da
EmekaOrji <emekapraiseo@gmail.com>
2022-06-29 06:14:39 -0700

    fix: betty - distributed grouped parameter description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 
 /**
  * add - adds two integers
- * num1, num2: the integers to be added
+ * num1: inputed integer 1
+ * num2: inputed integer 2
  * Return: the result of the addition
  */
 int add(int num1, int num2)
