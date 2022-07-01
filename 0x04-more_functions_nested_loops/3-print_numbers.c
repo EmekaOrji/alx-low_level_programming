72fccbd450120eb2ca7137beb264a8106e610b74
EmekaOrji <emekapraiseo@gmail.com>
2022-07-01 02:04:20 -0700

    fix: it's a char not an integer

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,14 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
  */
 void print_numbers(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = '0'; n <= '9'; n++)
 		_putchar('n');
 	_putchar('\n');
 }
