6d9ef614cb330044b485d1b9c6a08756e32d836d
EmekaOrji <emekapraiseo@gmail.com>
2022-07-01 03:57:41 -0700

    fix: betty

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a line in the terminal
- * @n - the number of underscores(_) to be printed
+ * @n: the number of underscores(_) to be printed
  * Return: void
  */
 void print_line(int n)
@@ -12,6 +12,5 @@ void print_line(int n)
 	if (n > 0)
 		for (i = 0; i < n; i++)
 			_putchar('_');
-
 	_putchar('\n');
 }
