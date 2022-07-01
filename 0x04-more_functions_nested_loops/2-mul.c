104f68bc16b08945775f65f64512d9bd3b0de6d7
EmekaOrji <emekapraiseo@gmail.com>
2022-07-01 01:50:15 -0700

    fix: betty

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
 /**
  * mul - multiplies two integers
  * @a: first integer to be multiplied
- * @b second integer to be multiplied
+ * @b: second integer to be multiplied
  * Return: the result of the multiplication
  */
 int mul(int a, int b)
