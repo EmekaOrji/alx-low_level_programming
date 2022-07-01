8aea4dacfede2a14e728b693e049bbafbc57d2a0
EmekaOrji <emekapraiseo@gmail.com>
2022-07-01 03:17:30 -0700

    fix: declared undeclared variable i

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,10 +3,12 @@
 /**
  * print_line - draws a line in the terminal
  * @n - the number of underscores(_) to be printed
- * Return: Always 0 (success)
+ * Return: void
  */
 void print_line(int n)
 {
+	int i;
+
 	if (n > 0)
 		for (i = 0; i < n; i++)
 			_putchar('_');
