98ef7db632970b3e2484e14b9f981ff72f6996d6
EmekaOrji <emekapraiseo@gmail.com>
2022-07-01 02:33:16 -0700

    fix: it was a problem of char type vs integer type

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,16 +10,7 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 48; n < 58; n++)
-	{
-		if (n == 2 || n == 4)
-		{
-			break;
-		}
-		else
-		{
-			continue;
-		}
-		_putchar(n);
-	}
+		if (n != 50 && n != 52)
+			_putchar(n);
 	_putchar('\n');
 }
