09bf4dafa45b398cd8f7347e11ebd72e8073b075
EmekaOrji <emekapraiseo@gmail.com>
2022-07-01 00:56:20 -0700

    Started a new project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to be checked
+ * Return: 1 if uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
+		return (0);
+	else
+		return;
+}
