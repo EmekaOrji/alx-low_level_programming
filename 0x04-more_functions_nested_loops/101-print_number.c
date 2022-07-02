42fbe7991eeeb2e11aaaaf3a84ab3b19cc751b81
EmekaOrji <emekapraiseo@gmail.com>
2022-07-02 02:06:50 -0700

    This one!

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number to the terminal
+ * @n: number to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	int n0, n1, n2, n3 = n0 = n1 = n2 = n, count, digit, i;
+
+	for (count = 0; n0 != 0; count++)
+		n0 = n0 / 10;
+
+	if (count > 1)
+		for (digit = count; digit > 1; digit--)
+		{
+			n1 = n3;
+			for (i = 1; i < digit; i++)
+				n1 = n1 / 10;
+			n1 = n1 % 10;
+			putchar(n1 + '0');
+		}
+
+	putchar((n2 % 10) + '0');
+}
